877e80adc2f5cb36d4e5eee761cf3bdd4bbc306c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-05-07 17:53:42 +0300

    работает перемещение в глубь директории

--- FileManagerProject/Form1.Designer.cs
+++ FileManagerProject/Form1.Designer.cs
@@ -36,18 +36,19 @@ namespace FileManagerProject
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.lbl_curdir = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(200, 12);
+            this.comboBox1.Location = new System.Drawing.Point(1, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(200, 38);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_EnterPushed);
             // 
             // listBox1
             // 
@@ -56,39 +57,51 @@ namespace FileManagerProject
             this.listBox1.ItemHeight = 25;
             this.listBox1.Location = new System.Drawing.Point(1, 56);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(600, 479);
+            this.listBox1.Size = new System.Drawing.Size(540, 479);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(661, 12);
+            this.textBox1.Location = new System.Drawing.Point(600, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 33);
+            this.textBox1.Size = new System.Drawing.Size(272, 33);
             this.textBox1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(600, 12);
+            this.label1.Location = new System.Drawing.Point(545, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Имя";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_curdir
+            // 
+            this.lbl_curdir.AutoSize = true;
+            this.lbl_curdir.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_curdir.Location = new System.Drawing.Point(208, 12);
+            this.lbl_curdir.Name = "lbl_curdir";
+            this.lbl_curdir.Size = new System.Drawing.Size(0, 30);
+            this.lbl_curdir.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.lbl_curdir);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "File Manager";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +113,7 @@ namespace FileManagerProject
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_curdir;
     }
 }
 
--- FileManagerProject/Form1.cs
+++ FileManagerProject/Form1.cs
@@ -13,16 +13,23 @@ namespace FileManagerProject
 {
     public partial class Form1 : Form
     {
+        List<string> current_path = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
             InitForm();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)  // выпадающщий список с именем диска
         {
             string root_dir = @$"{this.comboBox1.SelectedItem}";
+            current_path.Clear();
+            current_path.Add(root_dir[..(root_dir.Length - 1)]);
+
             string[] directories = Directory.GetDirectories(root_dir);
+            string[] files = Directory.GetFiles(root_dir);
+            
             List<string> output_dirs = new List<string>();
             int root_dir_size = root_dir.Length;
             for (int i = 0; i < directories.Length; i += 1)
@@ -30,13 +37,18 @@ namespace FileManagerProject
                 if (!directories[i].Contains("$"))
                     output_dirs.Add(directories[i][root_dir_size..]);
             }
+            for (int i = 0; i < files.Length; i += 1)
+                output_dirs.Add(files[i][root_dir_size..]);
+
+            this.listBox1.Items.Clear();
             this.listBox1.Items.AddRange(output_dirs.ToArray());
         }
 
         private void InitForm()
         {
             DriveInfo[] drives = DriveInfo.GetDrives();
-            this.comboBox1.Items.AddRange(drives);
+            foreach (var elem in drives)
+                this.comboBox1.Items.Add(elem.ToString()[..elem.ToString().Length]);
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -45,9 +57,47 @@ namespace FileManagerProject
             this.textBox1.Text = value;
         }
 
-        private void comboBox1_EnterPushed(object sender, EventArgs e)
+        private void EnterPushed(object sender, EventArgs e)
         {
-            MessageBox.Show("here");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string value = this.listBox1.SelectedItem.ToString();
+                current_path.Add(value);
+
+                string path = string.Join(@"\", current_path);
+                try
+                {
+                    string[] output_dirs = Directory.GetDirectories(path);
+                    string[] output_files = Directory.GetFiles(path);
+                    for (int i = 0; i < output_dirs.Length; i += 1)
+                        output_dirs[i] = output_dirs[i][(path.Length + 1)..];
+                    for (int i = 0; i < output_files.Length; i += 1)
+                        output_files[i] = output_files[i][(path.Length + 1)..];
+
+                    this.textBox1.Text = "";
+                    this.listBox1.Items.Clear();
+                    this.listBox1.Items.AddRange(output_dirs.ToArray());
+                    this.listBox1.Items.AddRange(output_files.ToArray());
+                } catch (IOException)
+                {
+                    
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
